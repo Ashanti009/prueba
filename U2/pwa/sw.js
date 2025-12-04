f48c6ddaf0837d887ee9b03ccacb1ef36c90da72
Ashanti009 <22307070@utcgg.edu.mx>
2025-12-04 11:31:48 -0600

    sw.js

--- U2/pwa/sw.js
+++ U2/pwa/sw.js
@@ -1,47 +1,48 @@
-//Nombre de la cache
+//nombre de la cache
 const CACHE_NAME = 'Media-PWA-v1';
 
-//Archivos que se almacenar en cache
+//archivos que se almacerean en cache
 const ASSETS = [
     './',
-    'index.html',
-    './Style.css',
-    'app.js',
-    'manifest.webmanifest',
-    'icons/icon-192.png',
-    'icons/icon-512.png',
-    'icons/maskable-192.png',
-    'icons/maskable-512.png'
+    './index.html',
+    './styles.css',
+    './app.js',
+    './manifest.webmanifest',
+    './icons/icon-192.png',
+    './icons/icon-512.png',
+    './icons/maskable-192.png',
+    './icons/maskable-512.png'
 ];
 
-//Evento que se ejecuta cuando el sw se instala por primera vez
+//evento que se ejecuta cuando el SW se instala por primera vez
 self.addEventListener('install', (e) => {
     self.skipWaiting();
     e.waitUntil(
-        caches.open(CACHE).then(c=> c.addAll(ASSETS))
+        caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))
     );
 });
 
-//Evento que se ejecuta cuando el sw se activa
+//evento que se ejecuta cuando el SW se activa
 self.addEventListener('activate', (e) => {
     e.waitUntil((async () => {
         const keys = await caches.keys();
-        (await Promise.all(k => k !== CACHE)).map(k => caches.delete(k));
+        await Promise.all(
+            keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+        );
         self.clients.claim();
     })());
 });
 
-//Evento que inercepta todas las peticiones de la red
+//evento que intercepta todas las peticiones de la aplicacion
 self.addEventListener('fetch', (e) => {
     const req = e.request;
     e.respondWith((async () => {
         const cached = await caches.match(req);
         if (cached) return cached;
-
         try {
             const fresh = await fetch(req);
-            const cache = await caches.open(CACHE);
-            if (req.method =='GET' && fresh.status == 200) {
+            const cache = await caches.open(CACHE_NAME);
+            if (req.method === 'GET' && fresh.status === 200) {
                 cache.put(req, fresh.clone());
             }
             return fresh;
@@ -50,5 +51,3 @@ self.addEventListener('fetch', (e) => {
         }
     })());
 });
-
-//
